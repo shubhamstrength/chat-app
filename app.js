806a97a651b93bd8a7cba0c27aae1e9af9632d40
bhuvanmalik007 <bhuvanmalik007@gmail.com>
2016-06-08 13:02:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -57,27 +57,14 @@ io.sockets.on('connection', function(socket){
 
 //NEW USER LOGS IN
   socket.on('new user', function(data,callback){
-    console.log(data);
     callback(true);
     socket.username=data;  ///******************** every socket is unique, therefore socket.username is unique
     users.push(socket.username);
     updateUsernames();
-    for(var t=0;t<users.length;t++) console.log("line65",users[t]);
   });
-  
-//socket.on('new namespace', function (data, callback) {
-//    var nsp = io.of(data);
-//    nsp.on('connection', function (socket) {
-
-//    });
-//    io.sockets.emit('send namespace', data);
-
-//});
 
   function updateUsernames(){
-    for(var t=0;t<users.length;t++) console.log("line78",users[t]);
-   // io.sockets.emit('get users',users);
-    io.sockets.emit('get users',connections);
+    io.sockets.emit('get users',users);
   }
 
 
